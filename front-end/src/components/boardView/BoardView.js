960d5ea48ca1ddcdbeec4ec8de75d94851a60b75
Lucas Berta <lucasberta59@gmail.com>
2022-11-28 00:31:31 +0000

    Board view drag and drop

--- front-end/src/components/boardView/BoardView.js
+++ front-end/src/components/boardView/BoardView.js
@@ -1,11 +1,14 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 import './BoardView.scss';
 
 import Task from './Task';
 import Button from '../../core/components/button/Button';
 import CreateEditBoardModal from './CreateEditBoardModal';
+import BoardApi from './../../core/api/board.api';
+import { refreshBoard } from './../../action/boardAction';
 
 // Components
 export function EmptyBoardView() {
@@ -18,28 +21,69 @@ export function EmptyBoardView() {
   );
 }
 
-function BoardColumn({ board, column }) {
+function BoardColumn({ column }) {
   return (
-    <div className='board-column'>
-      <h4>{`${column.name} (${column.tasks?.length || 0})`}</h4>
-      {board.tasks
-        ?.filter(task => task.status === column.name)
-        .map((task, index) => (
-          <Task key={index} task={task} />
-        ))}
-      <Button className='column-add-new-task' theme='secondary'>
-        + Add New Task
-      </Button>
-    </div>
+    <Droppable droppableId={column.name}>
+      {provided => (
+        <div {...provided.droppableProps} ref={provided.innerRef}>
+          <div className='board-column'>
+            <h4>{`${column.name} (${column?.tasks?.length || 0})`}</h4>
+            {column?.tasks?.map((task, index) => (
+              <Draggable key={task._id} draggableId={task._id} index={index}>
+                {provided => (
+                  <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                    <Task task={task} />
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+            <Button className='column-add-new-task' theme='secondary'>
+              + Add New Task
+            </Button>
+          </div>
+        </div>
+      )}
+    </Droppable>
   );
 }
 
-function BoardView({ boardState }) {
+function BoardView({ dispatch, boardState }) {
   const { selectedBoard } = boardState;
+  const [columns, setColumns] = useState({});
   const [createEditBoardModalOpen, setCreateEditBoardModalOpen] = useState(false);
+
+  useEffect(() => {
+    sortTasks();
+    function sortTasks(tasks) {
+      const _columns = {};
+      selectedBoard?.columns?.forEach(column => (_columns[column.name] = { name: column.name, tasks: [] }));
+      (tasks || selectedBoard?.tasks)?.forEach(task => {
+        _columns[task.status].tasks = [...(_columns[task.status]?.tasks || []), task];
+      });
+      setColumns(_columns);
+    }
+  }, [selectedBoard]);
 
   function isSelectedBoardEmpty() {
     return !selectedBoard?.columns || selectedBoard.columns?.length === 0;
+  }
+
+  function handleOnDragEnd(result) {
+    const _columns = { ...columns };
+    let [reorderedItem] = _columns[result.source.droppableId].tasks.splice(result.source.index, 1);
+    reorderedItem = { ...reorderedItem, status: result.destination.droppableId };
+    _columns[result.destination.droppableId].tasks.splice(result.destination.index, 0, reorderedItem);
+    setColumns(_columns);
+    const board = { ...selectedBoard };
+    let allTasks = [];
+    Object.keys(_columns).forEach(key => {
+      allTasks = [...allTasks, ..._columns[key].tasks];
+    });
+    board.tasks = allTasks;
+    BoardApi.updateBoard(board).then(response => {
+      dispatch(refreshBoard(response.data));
+    });
   }
 
   return (
@@ -58,15 +102,16 @@ function BoardView({ boardState }) {
         </div>
       )}
       {!isSelectedBoardEmpty() && (
-        <>
-          {selectedBoard.columns.map(column => (
-            <BoardColumn key={column.name} board={boardState.selectedBoard} column={column} />
-          ))}
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          {Object.keys(columns).map(key => {
+            return <BoardColumn key={key} column={columns[key]} />;
+          })}
           <div className='new-column' onClick={() => setCreateEditBoardModalOpen(true)}>
             <h1>+ New Column</h1>
           </div>
-        </>
+        </DragDropContext>
       )}
+
       <CreateEditBoardModal
         open={createEditBoardModalOpen}
         board={boardState.selectedBoard}
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -28,11 +28,11 @@ const routes = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={routes} />
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={routes} />
+  </Provider>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
